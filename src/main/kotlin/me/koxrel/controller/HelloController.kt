71deb6a8dcc552b67ed52ac4cd1c9c3ba82a932b
Igor Tresoumov <15571888+koxrel@users.noreply.github.com>
2021-10-09 11:34:12 +0300

    add test controller

--- src/main/kotlin/me/koxrel/controller/HelloController.kt
+++ src/main/kotlin/me/koxrel/controller/HelloController.kt
@@ -2,9 +2,13 @@ package me.koxrel.controller
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.PathVariable
 
 @Controller("/rest/hello")
 class HelloController {
     @Get
     fun getHello() = "Hello!"
+
+    @Get("/{name}")
+    fun getHelloByName(@PathVariable name: String) = "Hello, ${name}!"
 }
